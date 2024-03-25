f8b7d87fcbddc661bc7dca0a8cc312ce2932ae2f
MargheSerena <margheritaserena.ferrara@gmail.com>
2024-03-25 12:18:55 +0000

    str to lower in functions for EDA

--- EDA_functions/ratings_cleaning_steps.py
+++ EDA_functions/ratings_cleaning_steps.py
@@ -6,4 +6,11 @@ def remove_na_titles(
             subset = title_column, 
             axis = 0
             )
+
+def text_in_lower_case(
+            df, 
+            text_column):
+        # Make  lower case to avoid false duplicates
+        return df[text_column].str.lower()
+
             
