41a88d398dc94ab88c38ef653e889fe3e9c322c9
MargheSerena <margheritaserena.ferrara@gmail.com>
2024-03-25 13:12:17 +0000

    Update cleaning function

--- EDA_functions/ratings_cleaning_steps.py
+++ EDA_functions/ratings_cleaning_steps.py
@@ -17,13 +17,20 @@ def aggregate_df(
             df,
             group_by_columns,
             col_to_aggregate,
-            operation):
+            operation,
+            new_name_for_col_aggregated):
       if operation == 'average':
           return pd.DataFrame(
-                df.groupby(group_by_columns)[col_to_aggregate].mean()).reset_index()
+               df.groupby(group_by_columns)[col_to_aggregate].mean()
+                    ).reset_index().rename(
+                         columns = {col_to_aggregate: new_name_for_col_aggregated})
       if operation == 'median':
           return pd.DataFrame(
-                df.groupby(group_by_columns)[col_to_aggregate].median()).reset_index()
+               df.groupby(group_by_columns)[col_to_aggregate].median()
+                    ).reset_index().rename(
+                         columns = {col_to_aggregate: new_name_for_col_aggregated})
       if operation == 'count':
           return pd.DataFrame(
-                df.groupby(group_by_columns)[col_to_aggregate].count()).reset_index()
+               df.groupby(group_by_columns)[col_to_aggregate].count()
+                    ).reset_index().rename(
+                         columns = {col_to_aggregate: new_name_for_col_aggregated})
