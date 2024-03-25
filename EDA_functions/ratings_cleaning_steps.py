70171e7999a672b7db7d4a7a4463ce1dc60e176a
MargheSerena <margheritaserena.ferrara@gmail.com>
2024-03-25 12:28:14 +0000

    Update cleaning function

--- EDA_functions/ratings_cleaning_steps.py
+++ EDA_functions/ratings_cleaning_steps.py
@@ -13,4 +13,17 @@ def text_in_lower_case(
         # Make  lower case and remove double spaces to avoid false duplicates
         return df[text_column].str.lower().str.replace('  ', ' ')
 
-            
+def aggregate_df(
+            df,
+            group_by_columns,
+            col_to_aggregate,
+            operation):
+      if operation == 'average':
+          return pd.DataFrame(
+                df.groupby(group_by_columns)[col_to_aggregate].mean()).reset_index()
+      if operation == 'median':
+          return pd.DataFrame(
+                df.groupby(group_by_columns)[col_to_aggregate].median()).reset_index()
+      if operation == 'count':
+          return pd.DataFrame(
+                df.groupby(group_by_columns)[col_to_aggregate].count()).reset_index()
